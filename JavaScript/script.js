ba8b0dd02a9b4957f6d8d569ceaf477104d7d898
Evangelos R. <roudosisevang@gmail.com>
2026-08-27 18:35:03 +0300

    js content added

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,13 @@
-'use strict'
+'use strict';
+
+const deletePost = document.getElementById("delete_post");
+
+
+
+deletePost.addEventListener("click", function(e,){
+  e.preventDefault();
+  const postEl = document.querySelector(".post");
+  postEl.classList.add('delete_post');
+
+  
+});
