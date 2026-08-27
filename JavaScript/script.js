5c580e44960f49f7a0688e8ccd90f8de1b589fd6
Evangelos R. <roudosisevang@gmail.com>
2026-08-27 19:29:11 +0300

    JavaScript Functinality update

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,13 +1,43 @@
 'use strict';
 
-const deletePost = document.getElementById("delete_post");
+const deletePost1 = document.getElementById("delete_post1");
+const deletePost2 = document.getElementById("delete_post2");
+const deletePost3 = document.getElementById("delete_post3");
+const meContainer = document.querySelector(".me-container");
+const meBtn = document.querySelector("#me-btn");
 
 
 
-deletePost.addEventListener("click", function(e,){
+meBtn.addEventListener("click", function(e) {
   e.preventDefault();
-  const postEl = document.querySelector(".post");
-  postEl.classList.add('delete_post');
-
-  
+       
+      if (meContainer.classList.contains('hidden') || meContainer.style.display !== 'block') {
+          meContainer.style.display = 'block';
+          meContainer.classList.remove('hidden');
+    } else {
+          meContainer.style.display = 'none';
+          meContainer.classList.add('hidden');
+     }
+       
 });
+
+
+
+deletePost1.addEventListener("click", function(e){
+  e.preventDefault();
+  const postEl = document.querySelector(".post1");
+  postEl.classList.add('delete_post');
+});
+
+deletePost2.addEventListener("click", function(e){
+  e.preventDefault();
+  const postEl = document.querySelector(".post2");
+  postEl.classList.add('delete_post');
+});
+
+deletePost3.addEventListener("click", function(e){
+  e.preventDefault();
+  const postEl = document.querySelector(".post3");
+  postEl.classList.add('delete_post');
+});
+
